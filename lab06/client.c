b5d1a68ecdd35e71f8b89d2807b6d8d3521d0fb2
Sorina Sandu <sorina.sandu@ymail.com>
2012-03-29 14:46:59 +0300

    Uploaded Lab 06.

--- lab06/client.c
+++ lab06/client.c
@@ -4,7 +4,30 @@
 *	client mini-server de backup fisiere
 */
 
+/*
+
+	myaddr.famil7 - afinet
+	port = htons
+	sinaddr.s_addr = inet_addr(IP)
+	inet = htonl?
+
+
+*/
+
 #include "helpers.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 void usage(char*file)
 {
@@ -24,28 +47,62 @@ int main(int argc,char**argv)
 	struct sockaddr_in to_station;
 	char buf[BUFLEN];
 
+	int sfd,err,bytes_read,i=0;
+	char packetype=1;
+	char filename[20];
+	strcpy(filename, argv[3]);
+
 
 	/*Deschidere socket*/
-	
+	sfd = socket(PF_INET, SOCK_DGRAM, 0);
+	DIE( sfd == -1, "open socket" );
+
 	
 	/* Deschidere fisier pentru citire */
 	DIE((fd=open(argv[3],O_RDONLY))==-1,"open file");
 	
 	/*Setare struct sockaddr_in pentru a specifica unde trimit datele*/
-	
+	to_station.sin_family = AF_INET;
+	to_station.sin_port = htons(atoi(argv[2]));
+	to_station.sin_addr.s_addr = inet_addr(argv[1]);
+	//to_station.sin_addr.s_addr = hotnl(to_station.sin_addr.s_addr);
 	
 	
 	/*
 	*  cat_timp  mai_pot_citi
 	*		citeste din fisier
 	*		trimite pe socket
-	*/	
+	*/
+	bytes_read = 1+strlen(filename)+1;
+	memcpy(buf,&packetype,1);
+	memcpy(buf+1,filename,strlen(filename)+1);
+
+	while(bytes_read > 1){
+
+		err = sendto(sfd, buf, bytes_read, 0,  (struct sockaddr *)&to_station, sizeof(struct sockaddr));
+		DIE(err==-1, "sending");
+
+		if(i<5)
+			printf("\nSent %dth packet of len:%d /w type:%d",i, bytes_read, buf[0]);
+		if(i==0)
+			printf("\nbuf[0]:%d , buf+1:%s", buf[0], buf+1);
+		i++;
+
+		packetype = 0;
+		memcpy(buf,&packetype,1);
+		bytes_read = read(fd,buf+1,BUFLEN-1);
+		bytes_read += 1;
+		
+	}
+	err = sendto(sfd, buf, bytes_read, 0,  (struct sockaddr *)&to_station, sizeof(struct sockaddr));
+	DIE(err==-1, "sending");
 
 
 	/*Inchidere socket*/
-
+	close(sfd);
 	
 	/*Inchidere fisier*/
+	close(fd);
 
 	return 0;
 }
--- lab06/server.c
+++ lab06/server.c
@@ -4,51 +4,84 @@
 *	mini-server de backup fisiere
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+
 #include "helpers.h"
 
 
 void usage(char*file)
 {
-	fprintf(stderr,"Usage: %s server_port file\n",file);
-	exit(0);
+  fprintf(stderr,"Usage: %s server_port file\n",file);
+  exit(0);
 }
 
 /*
-*	Utilizare: ./server server_port nume_fisier
+* Utilizare: ./server server_port nume_fisier
 */
 int main(int argc,char**argv)
 {
-	if (argc!=3)
-		usage(argv[0]);
-	
-	struct sockaddr_in my_sockaddr,from_station ;
-	char buf[BUFLEN];
+  int fd;
+  if (argc != 3)
+    usage(argv[0]);
+
+  struct sockaddr_in my_sockaddr,from_station ;
+  socklen_t adr_len = 0;
+  char buf[BUFLEN];
 
 
-	/*Deschidere socket*/
-	
-	
-	/*Setare struct sockaddr_in pentru a asculta pe portul respectiv */
+  /*Deschidere socket*/
+  int sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
-	
-	/* Legare proprietati de socket */
+  /*Setare struct sockaddr_in pentru a asculta pe portul respectiv */
+  my_sockaddr.sin_family = AF_INET;
+  my_sockaddr.sin_port = htons(atoi(argv[1]));
+  my_sockaddr.sin_addr.s_addr = inet_addr("172.16.4.59");
 
-	
-	
-	/* Deschidere fisier pentru scriere */
-	DIE((fd=open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,0644))==-1,"open file");
-	
-	/*
-	*  cat_timp  mai_pot_citi
-	*		citeste din socket
-	*		pune in fisier
-	*/
+  /* Legare proprietati de socket r*/
+  bind(sockfd, (struct sockaddr *) &my_sockaddr, sizeof(my_sockaddr));
+
+  /* Deschidere fisier pentru scriere */
+  DIE((fd=open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,0644))==-1,"open file");
+
+  /*
+  * cat_timp  mai_pot_citi
+  * citeste din socket
+  * pune in fisier
+  */
+  int lg;
+  char file_name[BUFLEN];
+
+  while ((lg = recvfrom(sockfd, buf, BUFLEN, 0,
+                  (struct sockaddr *) &from_station,
+                  &adr_len)) > 0) {
+
+    if (buf[0] == 1) {
+      printf("%d\n", lg);
+      close(fd);
+      memcpy(file_name, buf + 1, lg - 1);
+      file_name[lg - 1] = 0;
+      puts(file_name);
+      DIE((fd=open(file_name, O_WRONLY|O_CREAT|O_TRUNC, 0644))==-1,"open file");
+    }
+    else
+      write(fd, buf + 1, lg - 1);
+  }
 
 
-	/*Inchidere socket*/	
+  /*Inchidere socket*/
+  close(sockfd);
 
-	
-	/*Inchidere fisier*/
+  /*Inchidere fisier*/
+  close(fd);
 
-	return 0;
+  return 0;
 }
